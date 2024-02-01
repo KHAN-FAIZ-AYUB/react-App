0b046df6e89e408e556f94fc59aeb97955255de1
Khan Faiz <khan.faiz.ayub@gmail.com>
2024-02-01 20:57:02 +0530

    passing children

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,26 @@
-import ListGroup from "./components/ListGroup";
-
+// import ListGroup from "./components/ListGroup";
+import Alert from "./components/Alert";
 function App() {
+  /*
   const items = ["Mumbai", "Delhi", "Bangluru", "Goa"];
   const handleSelectItem = (item: string) => {
     console.log(item);
   };
+  */
   return (
+    /*
     <div>
       <ListGroup
         items={items}
         heading="cities"
         onSelectItem={handleSelectItem}
       />
+    </div>
+*/
+    <div>
+      <Alert>
+        Hello<samp>world</samp>
+      </Alert>
     </div>
   );
 }
