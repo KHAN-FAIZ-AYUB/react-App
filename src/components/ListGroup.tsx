cf46c607abf135f7d2e22e2c879b451878310192
Khan Faiz <khan.faiz.ayub@gmail.com>
2024-02-01 20:01:37 +0530

    managing state

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,18 +1,31 @@
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
 
 function ListGroup() {
   const items = ["Mumbai", "Delhi", "Bangluru", "Goa"];
 
   //   Event handler
-  const handleClick = (event: MouseEvent) => console.log(event);
+  //   const handleClick = (event: MouseEvent) => console.log(event);
+
+  // state Hook
+  const [selectedIndex, setSelectedIndex] = useState(-1);
 
   return (
     <Fragment>
       <h1>List</h1>
       {items.length === 0 && <p>No items found</p>}
       <ul className="list-group">
-        {items.map((item) => (
-          <li className="list-group-item" key={item} onClick={handleClick}>
+        {items.map((item, index) => (
+          <li
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={item}
+            onClick={() => {
+              setSelectedIndex(index);
+            }}
+          >
             {item}
           </li>
         ))}
