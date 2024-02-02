b224852717b85e9ed34250163d9edbc4d8d947b9
Khan Faiz <khan.faiz.ayub@gmail.com>
2024-02-02 15:53:52 +0530

    initial

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 // import ListGroup from "./components/ListGroup";
-import Alert from "./components/Alert";
+import Button from "./Button";
+// import Alert from "./components/Alert";
 function App() {
   /*
   const items = ["Mumbai", "Delhi", "Bangluru", "Goa"];
   const handleSelectItem = (item: string) => {
     console.log(item);
   };
-  */
+
   return (
-    /*
     <div>
       <ListGroup
         items={items}
@@ -16,11 +16,24 @@ function App() {
         onSelectItem={handleSelectItem}
       />
     </div>
+    )
 */
+
+  /*
+  return (
     <div>
       <Alert>
         Hello<samp>world</samp>
       </Alert>
+    </div>
+  );
+  */
+
+  return (
+    <div>
+      <Button color="secondary" onClick={() => console.log("Clicked")}>
+        MY Button
+      </Button>
     </div>
   );
 }
